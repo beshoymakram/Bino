7e98c9d24673dd1f34878dcc3e25991bf53365ae
Beshoy <83455686+beshoymakram@users.noreply.github.com>
2021-09-24 18:40:42 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel();
+});
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    },
+})
+$(".owl-prev").html('<i class="fas fa-angle-left"></i>');
+$(".owl-next").html('<i class="fas fa-angle-right"></i>');
+$(".owl-item").removeClass('active');
